f7688effda8e74fe3748a2ef03acafde4bd1afbf
catmellowz <giift19@gmail.com>
2022-12-22 15:34:16 +0700

    import bootstrap and font

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AddButton from './coponents/AddButton';
 
 function App() {
+const data = [
+  {id, title: 'Learning', completed: true},
+  {id, title: 'Shopping', completed: false}
+]
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="container pt-5" style={{ maxWidth: 576 }}>
+      
+      <form>
+        <div className="input-group">
+          <input type="text" className="form-control" />
+          <button className="btn btn-primary">
+            <i className="fa-solid fa-check" />
+          </button>
+          <button className="btn btn-secondary">
+            <i className="fa-solid fa-xmark" />
+          </button>
+        </div>
+      </form>
+      <br />
+      <ul className="list-group">
+        <li className="list-group-item p-3 text-bg-danger">
+          <div className="d-flex align-items-center">
+            <span className="flex-fill">Play game</span>
+            <div className="btn-group">
+              <button className="btn btn-outline-light">
+                <i className="fa-solid fa-repeat" />
+              </button>
+              <button className="btn btn-outline-light">
+                <i className="fa-regular fa-trash-can" />
+              </button>
+            </div>
+          </div>
+        </li>
+        <li className="list-group-item p-3">
+          <form>
+            <div className="input-group">
+              <input type="text" className="form-control" value="Play game" />
+              <button className="btn btn-primary">
+                <i className="fa-solid fa-check" />
+              </button>
+              <button className="btn btn-secondary">
+                <i className="fa-solid fa-xmark" />
+              </button>
+            </div>
+          </form>
+        </li>
+        <li className="list-group-item p-3 text-bg-success">
+          <div className="d-flex align-items-center">
+            <span className="flex-fill">Meet the dentist</span>
+            <div className="btn-group">
+              <button className="btn btn-outline-light">
+                <i className="fa-solid fa-repeat" />
+              </button>
+              <button className="btn btn-outline-light">
+                <i className="fa-regular fa-trash-can" />
+              </button>
+            </div>
+          </div>
+        </li>
+      </ul>
     </div>
   );
+  
 }
 
 export default App;
